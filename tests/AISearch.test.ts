4f786d9f78da66d8ce69c91ef9ced4322415fdf5
Aurimas Stecenka <aurimas.stecenka@hostinger.com>
2024-11-15 09:10:06 +0200

    Workshop progress

--- tests/AISearch.test.ts
+++ tests/AISearch.test.ts
@@ -1,7 +1,87 @@
+import { createTestingPinia } from '@pinia/testing';
+import userEvent from '@testing-library/user-event';
 import { render } from '@testing-library/vue';
 
 import AISearch from '@/AISearch.vue';
 
-const renderComponent = () => render(AISearch);
+jest.mock('vue-router', () => ({
+  ...jest.requireActual('vue-router'),
+  useRouter: () => ({
+    resolve: () => ({
+      name: 'path-name',
+    }),
+  }),
+}));
+jest.mock('@/composables/useScreen', () => ({
+  useScreen: () => ({
+    isSmall: false,
+  }),
+}));
+jest.mock('@vueuse/core', () => ({
+  ...jest.requireActual('@vueuse/core'),
+  useDebounceFn: (callback: Function) => callback,
+}));
+jest.mock('@/repositories/aiSearchRepo', () => ({
+  getAISearch: () => [
+    {
+      data: {
+        documents: [
+          {
+            score: 1,
+            pageContent: 'page content',
+            metadata: {
+              breadcrumbs: 'breadcumbs',
+              subheader: 'this is the result',
+              path: '/path',
+            },
+          },
+        ],
+      },
+    },
+  ],
+}));
 
-describe('AISearch', () => {});
+const mockPinia = createTestingPinia({ stubActions: false });
+
+const renderComponent = (pinia = mockPinia) =>
+  render(AISearch, {
+    global: {
+      plugins: [pinia],
+    },
+  });
+
+describe('AISearch', () => {
+  describe('"AI Search trigger" button', () => {
+    it('should expand AI search on click', async () => {
+      const user = userEvent.setup();
+      const { getByRole } = renderComponent();
+
+      const button = getByRole('button');
+
+      await user.click(button);
+
+      const legend = getByRole('group', { name: 'What are you looking for?' });
+
+      expect(legend).toBeVisible();
+    });
+  });
+
+  it('should display search results on successful search input', async () => {
+    const user = userEvent.setup();
+    const pinia = createTestingPinia({
+      initialState: {
+        hpanelControlsStore: {
+          isAISearchOpen: true,
+        },
+      },
+    });
+    const { getByRole, queryByText, getByText } = renderComponent(pinia);
+
+    expect(queryByText('this is the result')).toBeNull();
+    const input = getByRole('textbox');
+
+    await user.type(input, 'abc');
+
+    expect(getByText('this is the result')).toBeVisible();
+  });
+});
